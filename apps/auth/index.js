f923b2af798ac0bae814b1a701f238918c605211
ajithvnr2001 <74783525+ajithvnr2001@users.noreply.github.com>
2026-02-17 07:54:16 +0000

    trigger2

--- apps/auth/index.js
+++ apps/auth/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-
+// Mock authentication endpoint
 app.get('/', (req, res) => {
   res.send('Auth Service v1.0');
 });
